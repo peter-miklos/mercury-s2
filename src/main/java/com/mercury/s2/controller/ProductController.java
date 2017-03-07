aeb3188a756843e61056e975b348da914930b3b5
Peter Miklos <pt.miklos@gmail.com>
2017-03-07 19:45:52 +0000

    exception handling added to get product method and get products method added

--- src/main/java/com/mercury/s2/controller/ProductController.java
+++ src/main/java/com/mercury/s2/controller/ProductController.java
@@ -4,25 +4,66 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.HttpStatus;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import com.mercury.s2.domain.Product;
 
 @RestController
 public class ProductController {
 
-  Product product = new Product();
+  Product product;
+  ArrayList<Product> products = new ArrayList<Product>();
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  void handleBadRequests(HttpServletResponse response) throws IOException {
+      response.sendError(HttpStatus.BAD_REQUEST.value(), "Please try again and with a non empty string as 'name'");
+  }
 
   @GetMapping(value = "/product/{product_id}")
-  public Product getProduct(@PathVariable Long product_id){
+  public Product getProduct(@PathVariable Long product_id) {
+    product = new Product();
 
-    product.setId(product_id);
-    product.setCategory("category1");
-    product.setGroup("group1");
-    product.setName("Test product");
+    for(Product productElem : products) {
+      if(productElem.getId() == product_id) {
+        product = productElem;
+      }
+    }
+
+    if (product_id == null || product.getId() == null) {
+      throw new IllegalArgumentException();
+    }
+
+    return product;
+  }
+
+  @GetMapping(value = "/products")
+  public ArrayList<Product> getProducts() {
+    product = new Product();
+    product.setId(Long.valueOf(11));
+    product.setCategory("category11");
+    product.setGroup("group11");
+    product.setName("Test product 11");
     product.setPrice(11.97);
     product.setOrigin("UK");
 
-    return product;
+    products.add(product);
+
+    product = new Product();
+    product.setId(Long.valueOf(12));
+    product.setCategory("category12");
+    product.setGroup("group12");
+    product.setName("Test product 12");
+    product.setPrice(4.97);
+    product.setOrigin("USA");
+
+    products.add(product);
+
+    return products;
+
   }
 
 }
