4eebbca27923827347a0fb1122a0cd7d13fb3e5e
Peter Miklos <pt.miklos@gmail.com>
2017-03-07 12:40:52 +0000

    basic spring-boot project added

---
+++ src/test/java/com/mercury/s2/MercuryS2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mercury.s2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MercuryS2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
