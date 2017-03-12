e7bb2a69a3943e816964d39c4466e01e57617fcc
Peter Miklos <pt.miklos@gmail.com>
2017-03-12 23:42:38 +0000

    add unit test for product update method

--- src/test/java/com/mercury/s2/service/product/ProductServiceTest.java
+++ src/test/java/com/mercury/s2/service/product/ProductServiceTest.java
@@ -15,21 +15,30 @@ public class ProductServiceTest {
   private ProductService productService;
   private ProductRepository productRepositoryMock;
   private Product productMock;
+  private Product originalProduct;
   private Product inputMock;
 
   @Before
   public void setUp() {
     productMock = mock(Product.class);
     inputMock = mock(Product.class);
+    originalProduct = mock(Product.class);
     when(inputMock.getProductName()).thenReturn("test product");
     productRepositoryMock = mock(ProductRepository.class);
     when(productRepositoryMock.save(any(Product.class))).thenReturn(inputMock);
+    when(productRepositoryMock.saveAndFlush(any(Product.class))).thenReturn(inputMock);
     productService = new ProductServiceImpl(productRepositoryMock);
   }
 
   @Test
   public void createCallsSaveOnProductRepositoryAndReturnsTheProduct() throws Exception {
     productMock = productService.create(inputMock);
+    assertEquals("Returned product has its name", productMock.getProductName(), "test product");
+  }
+
+  @Test
+  public void updateCallsSaveAndFlushOnProductRepositoryAndReturnsTheProduct() throws Exception {
+    productMock = productService.update(originalProduct, inputMock);
     assertEquals("Returned product has its name", productMock.getProductName(), "test product");
   }
 
