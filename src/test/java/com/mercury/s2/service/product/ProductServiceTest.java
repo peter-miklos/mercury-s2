e4969294a6eb9b8d0d45babddd6e7eacd0527625
Peter Miklos <pt.miklos@gmail.com>
2017-03-13 00:24:33 +0000

    add unit tests for getProductId method

--- src/test/java/com/mercury/s2/service/product/ProductServiceTest.java
+++ src/test/java/com/mercury/s2/service/product/ProductServiceTest.java
@@ -3,6 +3,8 @@ package com.mercury.s2.service.product;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import java.util.Optional;
+import java.util.Collection;
 
 import com.mercury.s2.repository.ProductRepository;
 import com.mercury.s2.domain.Product;
@@ -17,6 +19,7 @@ public class ProductServiceTest {
   private Product productMock;
   private Product originalProduct;
   private Product inputMock;
+  private Optional<Product> foundProductMock;
 
   @Before
   public void setUp() {
@@ -27,6 +30,8 @@ public class ProductServiceTest {
     productRepositoryMock = mock(ProductRepository.class);
     when(productRepositoryMock.save(any(Product.class))).thenReturn(inputMock);
     when(productRepositoryMock.saveAndFlush(any(Product.class))).thenReturn(inputMock);
+    when(productRepositoryMock.findOne(Long.valueOf(111))).thenReturn(inputMock);
+    when(productRepositoryMock.findOne(Long.valueOf(112))).thenReturn(null);
     productService = new ProductServiceImpl(productRepositoryMock);
   }
 
@@ -41,5 +46,18 @@ public class ProductServiceTest {
     productMock = productService.update(originalProduct, inputMock);
     assertEquals("Returned product has its name", productMock.getProductName(), "test product");
   }
+
+  @Test
+  public void getProductByIdCallsFindOneOnProductRepoWithValidId() throws Exception {
+    productMock = productService.getProductById(Long.valueOf(111)).get();
+    assertEquals("Returned product has its name", productMock.getProductName(), "test product");
+  }
+
+  @Test
+  public void getProductByIdCallsFindOneOnProductRepoWithInvalidId() throws Exception {
+    assertEquals("Null is returned", productService.getProductById(Long.valueOf(112)).isPresent(), false);
+  }
+
+
 
 }
